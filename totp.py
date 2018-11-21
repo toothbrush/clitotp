0ee4b57dfa8ff1d058be2e00f7b62ec89f8256ac
Paul David <paul@denknerd.org>
2018-11-21 21:04:48 +1100

    Nicer output

--- totp.py
+++ totp.py
@@ -14,17 +14,17 @@ user_wants = str(sys.argv[1])
 
 secret_file = os.path.join(my_secret_store_dir, user_wants)
 
-print("Will decrypt %s..." % (secret_file))
+print("Trying to decrypt %s..." % (secret_file)),
 
 with open(secret_file, "rb") as f:
     my_secret = gpg.decrypt_file(f)
     my_secret = str(my_secret).strip()
 
-print "Decrypted."
+print "done."
 
 my_token = otp.get_totp(my_secret)
 
-print my_token
 
+print("This token: %06d" % my_token),
 pyperclip.copy(my_token)
-print "Copied."
+print("(copied)")
